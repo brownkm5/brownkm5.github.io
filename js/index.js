da0e4af3379de93fedb97423afdb136da0a7d61b
Kevin Brown <brownkm5@g.cofc.edu>
2016-12-14 13:32:11 -0500

    adding about me

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function($){
-
+  var bio = "<p>My name is Kevin Brown, and I attended The Iron Yard in Greenville, SC.  I am familiar with HTML, CSS, SCSS, JavaScript, Backbone, Handlebars, & React.</p>"
   // sets up mobile nav bar motion
   $(function(){
     $('.button-collapse').sideNav();
@@ -20,4 +20,12 @@
     $('.caption').hide();
   });
 
+  function handleAboutMe(){
+    $('.about-me').replaceWith('<p>My name is Kevin Brown</p>');
+    console.log('working');
+  };
+
+  $('#about-button').on('click', function (){
+    $('.about-me').replaceWith(`<div class="row center-align"><h3>About Me</h3><div class="bio">${bio}</div></div>`);
+  });
 })(jQuery);
